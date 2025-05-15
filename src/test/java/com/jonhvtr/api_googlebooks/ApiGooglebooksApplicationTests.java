545cbab04c596bf3ffeb21ac56c0124f371d3179
jonhvtr <jonhvtr_dev@hotmail.com>
2025-05-14 23:38:25 -0300

    first commit

---
+++ src/test/java/com/jonhvtr/api_googlebooks/ApiGooglebooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonhvtr.api_googlebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGooglebooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
